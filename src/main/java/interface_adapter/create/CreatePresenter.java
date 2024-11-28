7bc485fe41d567e9f70619666ea6f002ac8fd843
Michael <michae0lsun@gmail.com>
2024-11-28 14:44:57 -0500

    checkstyle create Recipe

--- src/main/java/interface_adapter/create/CreatePresenter.java
+++ src/main/java/interface_adapter/create/CreatePresenter.java
@@ -1,12 +1,17 @@
 package interface_adapter.create;
 
+import java.util.HashMap;
+
 import interface_adapter.ViewManagerModel;
 import interface_adapter.recipe_search.RecipeSearchViewModel;
 import use_case.create.CreateOutputBoundary;
-import use_case.recipe_search.RecipeSearchOutputData;
 
-import java.util.HashMap;
-
+/**
+ * The CreatePresenter class serves as the presenter in the "Create Recipe" use case.
+ * It is responsible for updating the view models and notifying the UI of changes
+ * based on the outcome of the recipe creation process. It implements the
+ *  CreateOutputBoundary interface to handle success and failure scenarios.
+ */
 public class CreatePresenter implements CreateOutputBoundary {
     private final ViewManagerModel viewManagerModel;
     private final RecipeSearchViewModel recipeSearchViewModel;
