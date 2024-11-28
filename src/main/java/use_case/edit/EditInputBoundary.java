d52557ac371dbc47dcf3ba9bdc3964f4aaa8d314
94359 <zenghao.chen@mail.utoronto.ca>
2024-11-27 21:13:52 -0500

    fix checkstyle about usecase of Edite

--- src/main/java/use_case/edit/EditInputBoundary.java
+++ src/main/java/use_case/edit/EditInputBoundary.java
@@ -1,7 +1,13 @@
 package use_case.edit;
 
+/**
+ * Input Boundary for actions which are related to Edit.
+ */
 public interface EditInputBoundary {
 
+    /**
+     * Executes the switchToCreateView of Edit use case.
+     */
     void switchToCreateView();
     // from P4 to P6
 }
