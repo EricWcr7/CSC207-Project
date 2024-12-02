588427eff4202f9661b7860165d04d47502bb258
Quincygithub6 <129473729+Quincygithub6@users.noreply.github.com>
2024-12-02 13:14:17 -0500

    Change LikeRecipeInteractor and some other related files.

--- src/main/java/use_case/like_and_dislike_a_recipe/LikeAndDislikeRecipeDataAccessInterface.java
+++ src/main/java/use_case/like_and_dislike_a_recipe/LikeAndDislikeRecipeDataAccessInterface.java
@@ -18,32 +18,11 @@ public interface LikeAndDislikeRecipeDataAccessInterface {
     Recipe getOneRecipe(String recipeName);
 
     /**
-     * Deletes the recipe data file from the file I/O system.
-     * This method removes the existing recipe data file, typically as part of a process to replace
-     * it with an updated version.
-     */
-    void deleteFileFromFileIo();
-
-    /**
-     * Writes updated recipes to a file.
-     * This method saves the provided list of recipes to a data file for persistence.
-     * @param updatedRecipes the list of updated {@link Recipe} objects to be saved.
-     */
-    void writeRecipesToFile(List<Recipe> updatedRecipes);
-
-    /**
      * Retrieves the cached recipes from the data source.
      * This method fetches all recipes currently stored in the cache.
      * @return a list of {@link Recipe} objects representing the cached recipes.
      */
     List<Recipe> getCachedRecipes();
-
-    /**
-     * Uploads the recipe data file to the file I/O system.
-     * This method ensures the latest version of the recipe data file is stored and accessible
-     * within the file system.
-     */
-    void uploadFileToFileIo();
 
     /**
      * Updates the list of recipes with the changes provided.
